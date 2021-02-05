36d5e824356314a14e852b11958bef47d19aaf0d
KamikaziD <druhfus@gmail.com>
2021-02-05 09:24:51 +0200

    updated controllers security validation, moved Clarifai API to the backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'b6bdb851a0e34566ad2abafc88bfd3fd'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users')
@@ -11,5 +27,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
